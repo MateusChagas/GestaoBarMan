ad8343491c48378f0a1d2440675ca14c1ad40718
Mateus Oliveira <mateuschagasoliveira@gmail.com>
2020-01-24 14:41:35 -0300

    Layout completo

--- GestaoBarMan/Form1.Designer.vb
+++ GestaoBarMan/Form1.Designer.vb
@@ -25,6 +25,11 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.Login = New System.Windows.Forms.TextBox()
+        Me.Senha = New System.Windows.Forms.TextBox()
+        Me.Entrar = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SuspendLayout()
         '
         'Label1
@@ -59,11 +64,61 @@ Partial Class Form1
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Senha"
         '
+        'Login
+        '
+        Me.Login.Location = New System.Drawing.Point(107, 84)
+        Me.Login.Name = "Login"
+        Me.Login.Size = New System.Drawing.Size(206, 20)
+        Me.Login.TabIndex = 3
+        '
+        'Senha
+        '
+        Me.Senha.Location = New System.Drawing.Point(107, 139)
+        Me.Senha.Name = "Senha"
+        Me.Senha.Size = New System.Drawing.Size(206, 20)
+        Me.Senha.TabIndex = 4
+        '
+        'Entrar
+        '
+        Me.Entrar.BackColor = System.Drawing.SystemColors.Info
+        Me.Entrar.Location = New System.Drawing.Point(107, 212)
+        Me.Entrar.Name = "Entrar"
+        Me.Entrar.Size = New System.Drawing.Size(75, 23)
+        Me.Entrar.TabIndex = 5
+        Me.Entrar.Text = "Entrar"
+        Me.Entrar.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Info
+        Me.Button1.Location = New System.Drawing.Point(251, 212)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Cadastrar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(151, 172)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(104, 13)
+        Me.LinkLabel1.TabIndex = 7
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Esqueceu a Senha?"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
         Me.ClientSize = New System.Drawing.Size(475, 294)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Entrar)
+        Me.Controls.Add(Me.Senha)
+        Me.Controls.Add(Me.Login)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -76,5 +131,9 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
-
+    Friend WithEvents Login As TextBox
+    Friend WithEvents Senha As TextBox
+    Friend WithEvents Entrar As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
--- GestaoBarMan/Form1.vb
+++ GestaoBarMan/Form1.vb
@@ -3,4 +3,12 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Entrar_Click(sender As Object, e As EventArgs) Handles Entrar.Click
+
+    End Sub
 End Class
