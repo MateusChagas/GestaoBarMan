41ffbd73e67f8bd26f65dbc16e9134ea2500ed59
Mateus Oliveira <mateuschagasoliveira@gmail.com>
2020-01-27 20:01:13 -0300

    Resolução do problema de conexão

--- GestaoBarMan/GestaoBarMan/Form1.cs
+++ GestaoBarMan/GestaoBarMan/Form1.cs
@@ -25,19 +25,23 @@ namespace GestaoBarMan
             Funcionarios func = new Funcionarios();
             func.LoginFuncionario = Login.Text;
             func.Senha = Senha.Text;
-            MySqlConnection conexao = new MySqlConnection(ConfigurationSettings.AppSettings["conexao"]);            
+
+            if (String.IsNullOrEmpty(func.LoginFuncionario) || (String.IsNullOrEmpty(func.Senha)))
+            {
+                MessageBox.Show("É necessário informar usuário e senha!");
+            }  
+                   
+            MySqlConnection conexao = new MySqlConnection(ConfigurationSettings.AppSettings["conexao"]);
             MySqlCommand cmd = conexao.CreateCommand();
             cmd.CommandText = "SELECT * FROM FUNCIONARIOS WHERE LOGINFUNC = " + func.LoginFuncionario + "";
             conexao.Open();
-            cmd.CommandType = CommandType.Text;          
-            cmd.ExecuteNonQuery();
-            string result = cmd.CommandText;
-            conexao.Close();
-                        
-            if (result == "")
+            cmd.CommandType = CommandType.Text;        
+                      
             {
-                MessageBox.Show("O Usuário "+ func.LoginFuncionario + " não foi localizado ");
+                MessageBox.Show("O Usuário " + func.LoginFuncionario + " não foi localizado ");
             }
+            conexao.Close();
         }
+
     }
 }
